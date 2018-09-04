88dc39fd3a653bcd9793a30abdd5512ec14d963f
张强 <25720297@qq.com>
2018-09-04 12:25:34 +0800

    update

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,119 @@
+<template>
+
+  <div class="swipe-wrapper">
+    <div class="test">你好</div>
+    <i class="cubeic-alert"></i>
+    <cube-scroll>
+      <cube-swipe
+          @item-click="onItemClick"
+          @btn-click="onBtnClick"
+          :data="swipeData">
+      </cube-swipe>
+    </cube-scroll>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      toastTxt: 'cube toast content',
+      swipeData: [{
+        item: {
+          text: '测试1',
+          value: 1
+        },
+        btns: [
+          {
+            action: 'clear',
+            text: '不再关注',
+            color: '#c8c7cd'
+          },
+          {
+            action: 'delete',
+            text: '删除',
+            color: '#ff3a32'
+          }
+        ]
+      }, {
+        item: {
+          text: '测试2',
+          value: 2
+        },
+        btns: [
+          {
+            action: 'clear',
+            text: '不再关注',
+            color: '#c8c7cd'
+          },
+          {
+            action: 'delete',
+            text: '删除',
+            color: '#ff3a32'
+          }
+        ]
+      }, {
+        item: {
+          text: '测试3',
+          value: 3
+        },
+        btns: [
+          {
+            action: 'clear',
+            text: '不再关注',
+            color: '#c8c7cd'
+          },
+          {
+            action: 'delete',
+            text: '删除',
+            color: '#ff3a32'
+          }
+        ]
+      }]
+    }
+  },
+  methods: {
+    onItemClick (item) {
+      console.log('click item:', item)
+    },
+    onBtnClick (btn, index) {
+      if (btn.action === 'delete') {
+        this.$createActionSheet({
+          title: '确认要删除吗',
+          active: 0,
+          data: [
+            {content: '删除'}
+          ],
+          onSelect: () => {
+            this.swipeData.splice(index, 1)
+          }
+        }).show()
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.test {
+  width: 200px;
+  font-size: 12px;
+}
+</style>
